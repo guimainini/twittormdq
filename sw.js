4dc854c48f19bf024b70c2b271fccc52ad3ac590
Guillermo Mainini <guimainini@gmail.com>
2023-05-10 21:05:17 -0300

    modificaciones de ios

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 importScripts('./js/sw-util.js');
 
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamitic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamitic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -44,6 +44,9 @@ self.addEventListener('activate', e => {
         keys.forEach( key => {
             
             if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamitic') ) {
                 return caches.delete(key);
             }
         });
